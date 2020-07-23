86a4206c9e3b5dc3d43937845b0147e7c92feccc
d-sidler <fylwut-89>
2020-07-23 13:25:07 +0200

    Added itemTab.fxml

--- src/sample/CustomerController.java
+++ src/sample/CustomerController.java
@@ -1,9 +1,13 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,6 +16,31 @@ public class CustomerController implements Initializable {
 
     @FXML
     private ImageView user_placeholder_image;
+
+    @FXML
+    private VBox customerBox;
+
+    @FXML
+    private TextField customerFnameField;
+
+    @FXML
+    private TextField customerLnameField;
+
+    @FXML
+    private TextField customerEmailField;
+
+    @FXML
+    private Button customerSaveButton;
+
+    @FXML
+    void editAndSaveCustomer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void addCustomerButtonPressed(ActionEvent event) {
+
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
